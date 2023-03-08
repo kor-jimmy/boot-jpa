8374f246778c22eab2721d0000f012cc29d459e3
jintak.yoo <ascending77@naver.com>
2023-03-08 17:54:23 +0900

    도메인 분석 및 엔티티 생성

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+
+    // JPA 내장타입
+    @Embedded
+    private Address address;
+
+    // order 테이블에 있는 member에 의해 맵핑
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
+}
