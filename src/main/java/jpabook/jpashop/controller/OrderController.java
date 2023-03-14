a2e97279123a2a66e2b2b844a8295a8b940bd2a1
jintak.yoo <ascending77@naver.com>
2023-03-14 09:15:36 +0900

    주문 취소 추가

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -11,10 +11,7 @@ import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,14 +41,20 @@ public class OrderController {
                         @RequestParam("count") int count
     ) {
         orderService.order(memberId, itemId, count);
-        return "redirect:/oders";
+        return "redirect:/orders";
     }
 
     @GetMapping("/orders")
     public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
         List<Order> orders = orderService.findOrders(orderSearch);
-        model.addAttribute("orders", orders)
+        model.addAttribute("orders", orders);
         return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
     }
 
 }
