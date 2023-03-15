8001e80df663496ea47860557875784e54947e47
jintak.yoo <ascending77@naver.com>
2023-03-15 23:20:02 +0900

    회원 수정 api 작업

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,12 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,12 +22,23 @@ public class MemberApiController {
         return new CreateMemberResponse(memberId);
     }
 
-    @PostMapping("/api/v1/members")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
         Long memberId = memberService.join(member);
         return new CreateMemberResponse(memberId);
+    }
+
+
+    // 커맨드와 쿼리를 최대한 분리
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @Data
@@ -45,5 +55,18 @@ public class MemberApiController {
             this.id = id;
         }
     }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
 
 }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -18,11 +18,11 @@ import java.util.List;
 public class MemberService {
 
     /*
-    * 생성자 주입 방식을 권장
-    * 변경 불가능한 안전한 객체 생성 가능
-    * 생성자가 하나면, @Autowired 를 생략할 수 있다.
-    * final 키워드를 추가하면 컴파일 시점에 memberRepository 를 설정하지 않는 오류를 체크할 수 있다.
-    * */
+     * 생성자 주입 방식을 권장
+     * 변경 불가능한 안전한 객체 생성 가능
+     * 생성자가 하나면, @Autowired 를 생략할 수 있다.
+     * final 키워드를 추가하면 컴파일 시점에 memberRepository 를 설정하지 않는 오류를 체크할 수 있다.
+     * */
     private final MemberRepository memberRepository;
 
     @Transactional
@@ -48,6 +48,12 @@ public class MemberService {
 
     public Member findOne(Long id) {
         return memberRepository.findOne(id);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 
 }
