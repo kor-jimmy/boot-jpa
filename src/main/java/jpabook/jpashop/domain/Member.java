de77481d08757b0ed0fbafea1d76c04986dfa0f9
jintak.yoo <ascending77@naver.com>
2023-03-16 23:36:02 +0900

    지연 로딩, 조회 성능 최적화 테스트

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +25,7 @@ public class Member {
     private Address address;
 
     // order 테이블에 있는 member에 의해 맵핑
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
