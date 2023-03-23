9a1d6c4010587c002dc667c7e9982521d185ef67
jintak.yoo <ascending77@naver.com>
2023-03-23 23:22:55 +0900

    성능 최적화 테스트중 in절로 변경, n+1 문제 해결

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -75,6 +75,14 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDto();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+
+
+
     @Getter
     static class OrderDto {
         private Long orderId;
